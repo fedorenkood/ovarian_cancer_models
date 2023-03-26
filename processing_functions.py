77b526f1437549bc7b9c8e979bb7fbcbd2203ab8
fedorenkood <fedorenkoalex23@gmail.com>
2023-03-26 16:48:56 -0400

    fixes

--- processing_functions.py
+++ processing_functions.py
@@ -105,8 +105,7 @@ def performance_analysis(y_pred, y_prob, y_test, show_graph=True):
     report['class'] = report['class'].astype('int')
     report['dummy'] = 0
     report = merge_df_into_features(report, on_col='dummy', make_unique_over_cols=['class'])
-    report = report.drop(['dummy'], axis=1)
-    report = remove_featues_startswith(report, ['class'], show_removed=False)
+    report = remove_featues_startswith(report, ['class', 'dummy'], show_removed=False)
     report['accuracy'] = accuracy
     report['auc'] = auc
     return auc, accuracy, report
@@ -324,7 +323,7 @@ def df_filter_val_distribution_on_cancer(source_df, make_unique_over_cols, name,
     df_filter_val_distribution(df, 'cancer', make_unique_over_cols, name, hist=hist, cutoff_percentage=cutoff_percentage)
 
 
-def resample_max(df, label, n_max_per_class, replace=True):
+def resample_max(df, label, n_max_per_class, is_test=False, replace=True):
 
     df_majority = df[df[label] ==0]
     df_minority = df[df[label] ==1]
@@ -336,12 +335,14 @@ def resample_max(df, label, n_max_per_class, replace=True):
                                     # random_state=44
                                     ) 
 
-    # Upsample minority class
-    df_minority_upsampled = resample(df_minority, 
-                                    replace=replace,     # sample with replacement
-                                    n_samples=n_max_per_class,    # to match majority class
-                                    # random_state=44
-                                    ) 
+    df_minority_upsampled = df_minority
+    if not is_test:
+        # Upsample minority class
+        df_minority_upsampled = resample(df_minority, 
+                                        replace=replace,     # sample with replacement
+                                        n_samples=n_max_per_class,    # to match majority class
+                                        # random_state=44
+                                        ) 
 
     # Combine majority class with upsampled minority class
     df_sampled = pd.concat([df_majority_downsampled, df_minority_upsampled])
@@ -458,23 +459,76 @@ def merge_data_over_years(person_df, screen_df, abnorm_df, screen_join='left', a
             df.loc[index, label_feature] = 1
         df_final = pd.concat([df_final, df])
         df_final = df_final.drop_duplicates()
+    # Add a feature that says whether person was screened or not
+    condition = df_final['plco_id'].isin(screen_df['plco_id'])
+    df_final['was_screened'] = 0
+    df_final.loc[condition, 'was_screened'] = 1
     return df_final
 
 
 def impute_data(X_train: pd.DataFrame, X_test: pd.DataFrame, strategy, label):
+    # Fill ovarycyst variables with 0 if they are in screen dataset (e.g. were screened)
+    fill_const = {
+        'numcyst': 0,
+        'ovcyst_morph': 0,
+        'ovcyst_outline': 0,
+        'ovcyst_solid': 0,
+        'ovcyst_sum': 0,
+        'ovcyst_vol': 0,
+        'numcyst': 0,
+        'tvu_result': 1,
+        'numcystl': 0,
+        'numcystr': 0,
+        'ovcyst_diaml': 0,
+        'ovcyst_diamr': 0,
+        'ovcyst_morphl': 0,
+        'ovcyst_morphr': 0,
+        'ovcyst_outlinel': 0,
+        'ovcyst_outliner': 0,
+        'ovcyst_solidl': 0,
+        'ovcyst_solidr': 0,
+        'ovcyst_suml': 0,
+        'ovcyst_sumr': 0,
+        'ovcyst_voll': 0,
+        'ovcyst_volr': 0,
+        'visboth': 0,
+        'viseith': 0,
+        'visl': 0,
+        'visr': 0
+    }
+    const_val_cols = list(fill_const.keys())
+    # print(X_train[list(fill_const.keys())].describe().T)
+    # X_train.loc[X_train['was_screened'] == 1] = X_train.loc[X_train['was_screened'] == 1].fillna(fill_const)
+    # X_test.loc[X_test['was_screened'] == 1] = X_test.loc[X_test['was_screened'] == 1].fillna(fill_const)
+    X_train = X_train.fillna(fill_const)
+    X_test = X_test.fillna(fill_const)
+    X_train[const_val_cols] = X_train[const_val_cols].astype(np.int16)
+    X_test[const_val_cols]  = X_test[const_val_cols].astype(np.int16)
+    # Impute particular values with means
+    mean_imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    mean_imputer.fit(X_train[const_val_cols])
+    # print_df(X_train[const_val_cols].describe().T)
+    # print(X_test[const_val_cols].shape)
+    # print(mean_imputer.transform(X_test[const_val_cols]).shape)
+    X_test[const_val_cols] = mean_imputer.transform(X_test[const_val_cols])
+    X_train[const_val_cols] = mean_imputer.transform(X_train[const_val_cols])
+
+    # Others with whatever strategy we decide
     mean_imputer = SimpleImputer(missing_values=np.nan, strategy=strategy)
     mean_imputer.fit(X_train)
     X_test = mean_imputer.transform(X_test)
+    X_train = mean_imputer.transform(X_train)
 
     # labeled_impute = LabeledImmpute()
     # labeled_impute.fit(X_train, label)
     # X_train = labeled_impute.transform(X_train, label, strategy)
-    X_train = mean_imputer.transform(X_train)
     return X_train, X_test
 
 def process_train_test_split(source_df, train, test, label, train_size, test_size, strategy, stats=True):
     train = source_df[source_df['plco_id'].isin(train['plco_id'])]
     test = source_df[source_df['plco_id'].isin(test['plco_id'])]
+    # train = train.merge(source_df, how='left')
+    # test = test.merge(source_df, how='left')
     y_train = train[label]
     y_test = test[label]
     # Printing stats
@@ -494,39 +548,43 @@ def process_train_test_split(source_df, train, test, label, train_size, test_siz
     test = pd.DataFrame(test, columns=columns)
     # Perform oversamping
     train = resample_max(train, label, train_size, replace=True)
-    test = resample_max(test, label, test_size, replace=True)
+    # test = resample_max(test, label, test_size, replace=True)
 
     y_train = train[label]
     y_test = test[label]
-    X_train = train.drop([label], axis=1)
-    X_test = test.drop([label], axis=1)
+    X_train = train.drop([label, 'was_screened'], axis=1)
+    X_test = test.drop([label, 'was_screened'], axis=1)
     return X_train, X_test, y_train, y_test
 
-def process_and_impute_for_label_kfold(source_df, label, strategy, n_max_per_class=10000, k=10, stats=True):
+def process_and_impute_for_label_kfold(source_df, label, strategy, n_max_per_class=10000, num_folds=10, stats=True):
     # remove features starting with cancer so that we could drop labels that are nan (e.g. people get cancer later on)
     source_df = remove_featues_startswith(source_df, ['cancer_'], [label], show_removed=False)
     source_df = source_df[source_df[label].notnull()]
     # One person should not appear in train and test data since there are duplicates of a person
     # we splits of data on person id and then oversample from that sample 
-    unique_id_df = source_df[['plco_id', label]].drop_duplicates()
+    # TODO: this line of code determines whether the model is leaking info or not
+    unique_id_df = source_df[['plco_id', label]].drop_duplicates(subset='plco_id') # subset='plco_id'
     X_train_unique, X_test_unique, y_train, y_test = train_test_split(unique_id_df, unique_id_df[label], test_size = 0.2)
+    # print_records_vs_unique(X_train_unique, 'plco_id', f'Train set', print_vals=True)
+    # print_records_vs_unique(X_test_unique, 'plco_id', f'Test set', print_vals=True)
 
     # Printing stats
     print(f'Distribution of labels based on unique plco_id: {np.sum(y_test)/(np.sum(y_train) + np.sum(y_test))}')
     # print_df(X_train[X_train[label]==1][:100])
     train_size = int(n_max_per_class * 0.8)
     test_size  = int(n_max_per_class * 0.2)
-    train_fold_size  = int((k-1) * train_size / k)
-    test_fold_size  = int(train_size / k)
+    train_fold_size  = int((num_folds-1) * train_size / num_folds)
+    test_fold_size  = int(train_size / num_folds)
 
     train_test_lambda = lambda: process_train_test_split(source_df, X_train_unique, X_test_unique, label, train_size, test_size, strategy, stats=stats)
     # create list of lambdas for each fold
     # Cross validation: https://vitalflux.com/k-fold-cross-validation-python-example/
-    strtfdKFold = StratifiedKFold(n_splits=k)
+    strtfdKFold = StratifiedKFold(n_splits=num_folds)
     kfold = strtfdKFold.split(unique_id_df, unique_id_df[label])
     k_fold_lambdas = []
-    for train, test in kfold:
+    for k, (train, test) in enumerate(kfold):
         train = unique_id_df.iloc[train, :]
+        # print_records_vs_unique(train, 'plco_id', f'cv fold {k}', print_vals=True)
         test = unique_id_df.iloc[test, :]
         k_fold_lambdas.append(lambda: process_train_test_split(source_df, train, test, label, train_fold_size, test_fold_size, strategy, stats=False))
 
