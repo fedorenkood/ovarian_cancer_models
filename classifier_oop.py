44bd3cade4e141dc04ef60ecf108e65227e755c5
fedorenkood <fedorenkoalex23@gmail.com>
2023-04-11 14:42:20 -0400

    created ExperimentDataHelper

--- classifier_oop.py
+++ classifier_oop.py
@@ -567,3 +567,69 @@ class TrainTestSplitUtil:
             k_fold_lambdas.append(lambda: self.data_util.copy().process_train_test_split(self.source_df, train[self.data_util.id_col], test[self.data_util.id_col]))
 
         return k_fold_lambdas
+
+
+class ExperimentDataHelper:
+    def __init__(self, source_df: pd.DataFrame, label: str, other_lebels: List[str], imputer_util: ImputerUtil = None, data_util: ClassifierDataUtil = None, train_test_split_util: TrainTestSplitUtil = None) -> None:
+        self.source_df = source_df
+        self.label = label
+        self.other_lebels = other_lebels
+        self.imputer_util = imputer_util
+        self.data_util = data_util
+        self.train_test_split_util = train_test_split_util
+        self._process_source()
+
+        if not imputer_util:
+            self._init_imputer() 
+        if not data_util:
+            self._init_data_util()
+        if not train_test_split_util:
+            self._init_train_test_split_util()
+
+    def _process_source(self) -> None:
+        self.source_df = remove_featues_startswith(self.source_df, self.other_lebels, [self.label], show_removed=False).drop_duplicates()
+        self.source_df = self.source_df[self.source_df[self.label].notnull()]
+
+    def _init_imputer(self) -> None:
+        pass
+
+    def _init_data_util(self) -> None:
+        self.data_util = ClassifierDataUtil(self.label, self.imputer_util)
+
+    def _init_train_test_split_util(self) -> None:
+        self.train_test_split_util = TrainTestSplitUtil(self.source_df, self.data_util, False)
+        
+
+class ExperimentDataHelper1(ExperimentDataHelper):
+    def _init_imputer(self) -> None:
+        impute_const_dict = {
+            'numcyst': 0,
+            'ovcyst_morph': 0,
+            'ovcyst_outline': 0,
+            'ovcyst_solid': 0,
+            'ovcyst_sum': 0,
+            'ovcyst_vol': 0,
+            'numcyst': 0,
+            'tvu_result': 1,
+            'numcystl': 0,
+            'numcystr': 0,
+            'ovcyst_diaml': 0,
+            'ovcyst_diamr': 0,
+            'ovcyst_morphl': 0,
+            'ovcyst_morphr': 0,
+            'ovcyst_outlinel': 0,
+            'ovcyst_outliner': 0,
+            'ovcyst_solidl': 0,
+            'ovcyst_solidr': 0,
+            'ovcyst_suml': 0,
+            'ovcyst_sumr': 0,
+            'ovcyst_voll': 0,
+            'ovcyst_volr': 0,
+            'visboth': 0,
+            'viseith': 0,
+            'visl': 0,
+            'visr': 0
+        }
+        numeric_columns = select_numeric_columns(self.source_df)
+        numeric_columns = list(set(numeric_columns) - set(impute_const_dict.keys()))
+        self.imputer_util = ImputerUtil(impute_const_dict, impute_mean_cols=numeric_columns, impute_median_cols=[])
